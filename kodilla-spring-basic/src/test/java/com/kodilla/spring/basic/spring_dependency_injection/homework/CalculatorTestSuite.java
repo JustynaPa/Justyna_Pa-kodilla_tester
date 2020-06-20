a49271e2fac5b1290a91c6d7e3949a196d8fc497
Justyna Palewicz <justyna.palewicz@gmail.com>
2020-06-20 16:35:48 +0200

    Zadanie: kalkulator

---
+++ kodilla-spring-basic/src/test/java/com/kodilla/spring/basic/spring_dependency_injection/homework/CalculatorTestSuite.java
@@ -0,0 +1,42 @@
+package com.kodilla.spring.basic.spring_dependency_injection.homework;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class CalculatorTestSuite {
+
+    @Test
+    public void shouldAdded() {
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.basic");
+        Calculator bean = context.getBean(Calculator.class);
+        double testValue = bean.add(2,2);
+        Assertions.assertEquals(4, testValue);
+    }
+
+    @Test
+    public void shouldSubtracted() {
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.basic");
+        Calculator bean = context.getBean(Calculator.class);
+        double testValue = bean.subtract(2,10);
+        Assertions.assertEquals(-8, testValue);
+    }
+
+    @Test
+    public void shouldMultiplied() {
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.basic");
+        Calculator bean = context.getBean(Calculator.class);
+        double testValue = bean.multiply(0,10);
+        Assertions.assertEquals(0, testValue);
+    }
+
+    @Test
+    public void shouldDivided() {
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.basic");
+        Calculator bean = context.getBean(Calculator.class);
+        double testValue = bean.divide(50,100);
+        Assertions.assertEquals(0.5, testValue);
+    }
+
+}
