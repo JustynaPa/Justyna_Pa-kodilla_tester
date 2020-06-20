69c6d303e63ccc3da3f7dcc0ebb5e3387f3e5b51
Justyna Palewicz <justyna.palewicz@gmail.com>
2020-06-20 16:43:23 +0200

    Zadanie: kalkulator v2

--- kodilla-spring-basic/src/test/java/com/kodilla/spring/basic/spring_dependency_injection/homework/CalculatorTestSuite.java
+++ kodilla-spring-basic/src/test/java/com/kodilla/spring/basic/spring_dependency_injection/homework/CalculatorTestSuite.java
@@ -2,9 +2,11 @@ package com.kodilla.spring.basic.spring_dependency_injection.homework;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+@SpringBootTest
 public class CalculatorTestSuite {
 
     @Test
