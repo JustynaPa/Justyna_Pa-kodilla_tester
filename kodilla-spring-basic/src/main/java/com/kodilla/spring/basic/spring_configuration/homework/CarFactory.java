56cab5b4f173473cfd0e8ae595728c36b0d9ab49
Justyna Palewicz <justyna.palewicz@gmail.com>
2020-06-23 12:44:06 +0200

    Zadanie: Zadanie: warunkowe zwracanie beana v2

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_configuration/homework/CarFactory.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_configuration/homework/CarFactory.java
@@ -12,12 +12,16 @@ public class CarFactory {
     @Bean
     public Car dependingOnSeason() {
         Car car;
-        if (MonthDay.now().isAfter(MonthDay.of(6,20)) || MonthDay.now().isBefore(MonthDay.of(9,21))) {
-            car = new Cabrio();
-        } else if (MonthDay.now().isAfter(MonthDay.of(12,20)) || MonthDay.now().isBefore(MonthDay.of(3,21))) {
+        if ((MonthDay.now().isAfter(MonthDay.of(3,20))
+                && MonthDay.now().isBefore(MonthDay.of(6,21)))
+                || (MonthDay.now().isAfter(MonthDay.of(8,20))
+                && MonthDay.now().isBefore(MonthDay.of(12,21)))) {
+            car = new Sedan();
+        } else if (MonthDay.now().isAfter(MonthDay.of(12,20))
+                && MonthDay.now().isBefore(MonthDay.of(3,21))) {
             car = new SUV();
         } else {
-            car = new Sedan();
+            car = new Cabrio();
         }
         return car;
     }
