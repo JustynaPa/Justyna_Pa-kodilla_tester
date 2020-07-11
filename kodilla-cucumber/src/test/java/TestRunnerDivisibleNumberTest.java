a12cd7c41e55a98dd265dde5b02ca08950a3cdd1
Justyna Palewicz <justyna.palewicz@gmail.com>
2020-07-12 01:17:29 +0200

    Zadanie: używamy Cucumbera

---
+++ kodilla-cucumber/src/test/java/TestRunnerDivisibleNumberTest.java
@@ -0,0 +1,10 @@
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"src/test/resources/divisiblenumberchecker/is_it_divisible_number.feature"}
+)
+public class TestRunnerDivisibleNumberTest {
+}
